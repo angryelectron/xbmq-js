e5e08e0e902f9a77b5bb029a715b779ca52199cb
Andrew Bythell <abythell@ziptrek.com>
2016-01-22 15:52:34 -0800

    renamed

--- xbmq.js
+++ xbmq.js
@@ -1,63 +1,99 @@
-var Mqtt = require('mqtt');
+var q = require('q');
+var util = require('util');
+var Xbmq = require('./xbmq');
+var SerialPort = require('serialport').SerialPort;
+var xbee_api = require('xbee-api');
 
-module.exports = function (broker, rootTopic, receivedMessageCallback) {
+var xbmq = null;
+var C = xbee_api.constants;
+var xbeeAPI = new xbee_api.XBeeAPI({api_mode: 2});
+var serialport = new SerialPort('/dev/ttyUSB0', {
+    baudrate: 9600,
+    parser: xbeeAPI.rawParser()
+});
 
-    var xbmq = this;
-    xbmq.publishXBeeFrame = publishXBeeFrame;
+serialport.on('open', function () {
+    console.log('Serial Port Open');
+    startXbmq();
+});
 
-    var mqttOptions = {
-        will: {
-            topic: rootTopic + '/online',
-            payload: '0',
-            qos: 0,
-            retain: true
+serialport.on('error', function (error) {
+    console.log(error);
+});
+
+xbeeAPI.on('frame_object', function publishFrame(frame) {
+    if (xbmq) {
+        xbmq.publishXBeeFrame(frame);
+    }
+});
+
+/**
+ * Send a frame to the XBee network.  Intended to be used as a callback when
+ * MQTT receives new messages.
+ * @param {type} error
+ * @param {type} frame xbee-api frame 
+ */
+function xbmqCallback(error, frame) {
+    if (error) {
+        console.log(error);
+    } else {
+        serialport.write(xbeeAPI.buildFrame(frame));
+    }
+}
+
+/*
+ * Start XBMQ 
+ */
+function startXbmq() {
+    getGatewayAddress().then(function (address) {
+        rootTopic = 'ab123/' + address;
+        console.log('rootTopic: ' + rootTopic);
+        xbmq = new Xbmq('mqtt://fona.ziptrek.eco', rootTopic, xbmqCallback);
+    });
+}
+
+/*
+ * Send an XBee request and return a promise fulfilled with the response.
+ */
+function xbeeCommand(frame) {
+    var timeout = 5000;
+    frame.id = xbeeAPI.nextFrameId();
+    var deferred = q.defer();
+
+    var callback = function (receivedFrame) {
+        if (receivedFrame.id === frame.id) {
+            // This is our frame's response. Resolve the promise.
+            deferred.resolve(receivedFrame);
         }
     };
 
-    var mqtt = Mqtt.connect(broker, mqttOptions);
+    setTimeout(function () {
+        xbeeAPI.removeListener('frame_object', callback);
+    }, timeout + 1000);
 
-    function mqttIsOnline(isOnline) {
-        var message = isOnline ? '1' : '0';
-        var topic = rootTopic + '/online';
-        mqtt.publish(topic, message);
-    }
+    xbeeAPI.on('frame_object', callback);
+    serialport.write(xbeeAPI.buildFrame(frame));
+    return deferred.promise.timeout(timeout);
+}
 
-    mqtt.on('reconnect', function () {
-        mqttIsOnline(true);
+/**
+ * Return a promise that resolves with the 64-bit address of the local XBee 
+ * attached to this gateway.
+ */
+function getGatewayAddress() {
+    var gw = null;
+    var frame = {
+        type: C.FRAME_TYPE.AT_COMMAND,
+        commandParameter: []
+    };
+    frame.command = 'SH';
+    return xbeeCommand(frame)
+            .then(function (sh) {
+                gw = sh.commandData.toString('hex');
+                frame.command = 'SL';
+                return xbeeCommand(frame);
+            }).then(function (sl) {
+        gw += sl.commandData.toString('hex');
+        return gw;
     });
-
-    mqtt.on('close', function () {
-        mqttIsOnline(false);
-    });
-
-    mqtt.on('offline', function () {
-        mqttIsOnline(false);
-    });
-
-    mqtt.on('connect', function () {
-        console.log("MQTT connected.");
-        mqttIsOnline(true);
-        mqtt.subscribe(rootTopic + '/request'); //TODO: do subscriptions survive a reconnect?
-    });
-
-    mqtt.on('error', function (error) {
-        console.log(error);
-    });
-
-    mqtt.on('message', function (topic, message) {
-        console.log('Received: ' + topic + ': ' + message);        
-        try {
-            var frame = JSON.parse(message);
-            receivedMessageCallback(frame);
-        } catch (error) {
-            return receivedMessageCallback(error, message);
-        }
-        return receivedMessageCallback(null, frame);
-    });
-
-    function publishXBeeFrame(frame) {
-        var message = JSON.stringify(frame, null, 0);
-        mqtt.publish(rootTopic + '/response', message);
-    }
-};
-
+}
