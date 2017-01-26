286c56dce6babd9ede598ce5cc22ef6e73e0141f
Andrew Bythell <abythell@ieee.org>
2017-01-25 23:11:00 -0800

    use dependency injection

--- test/xbee-test.js
+++ test/xbee-test.js
@@ -1,57 +1,88 @@
-/* global describe it xit */
+/* global describe it beforeEach xit */
 var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 var expect = chai.expect
 
-var xbee = require('../lib/xbee.js')
+var XBee = require('../lib/xbee.js')
+var SerialPort = require('serialport')
+var VirtualSerialPort = require('virtual-serialport')
+var XBeeAPI = require('xbee-api')
 
-describe('xbee', function () {
-  describe('xbee.begin()', function () {
-    it('should reject if port is invalid', function () {
-      return expect(xbee.begin('/dev/null', 9600, 2, function (e, m) {}))
-        .to.eventually.be.rejectedWith(Error)
+describe('XBee', function () {
+  describe('XBee#create', function () {
+    var options
+
+    beforeEach(function () {
+      options = {
+        apiMode: 2,
+        port: '/dev/null',
+        baud: 9600,
+        callback: function (e, message) {}
+      }
     })
 
-    it('should reject if baud is invalid', function () {
-      return expect(xbee.begin('/dev/null', 'badbaud', 2, function (e, m) {}))
+    it('rejects if port is invalid', function () {
+      options.port = null
+      return expect(XBee.create(SerialPort, XBeeAPI, options))
+      .to.eventually.be.rejectedWith('Bad or missing arguments')
+    })
+
+    it('rejects if baud is invalid', function () {
+      options.baud = 'invalid'
+      return expect(XBee.create(SerialPort, XBeeAPI, options))
       .to.eventually.be.rejectedWith('Invalid "baudRate"')
     })
 
-    it('should reject if apiMode is invalid', function () {
-      return expect(xbee.begin('/dev/null', 9600, 'bad', function (e, m) {}))
+    it('rejects if apiMode is invalid', function () {
+      options.apiMode = 'invalid'
+      return expect(XBee.create(SerialPort, XBeeAPI, options))
       .to.eventually.be.rejectedWith('Invalid API mode')
     })
 
-    it('should reject if callback is invalid', function () {
-      return expect(xbee.begin('/dev/null', 9600, 2))
-      .to.eventually.be.rejectedWith('Bad or missing messageCallback')
+    it('rejects if callback is invalid', function () {
+      options.callback = null
+      return expect(XBee.create(SerialPort, XBeeAPI, options))
+      .to.eventually.be.rejectedWith('Bad or missing callback')
     })
 
-    xit('should reject if serial port cannot be opened', function () {
-
+    it('creates an instance of XBee', function () {
+      return XBee.create(VirtualSerialPort, XBeeAPI, options).then(function (xbee) {
+        expect(xbee).to.be.instanceof(XBee)
+      })
     })
 
-    xit('should open the serial port', function (done) {
-
+    it('rejects if serial port cannot be opened', function () {
+      return expect(XBee.create(SerialPort, XBeeAPI, options))
+      .to.eventually.be.rejectedWith('Unknown error code 3')
     })
 
-    xit('should call messageCallback with Error on XBee error', function (done) {
-
+    it('opens the serial port', function () {
+      return XBee.create(VirtualSerialPort, XBeeAPI, options).then(function (xbee) {
+        expect(xbee.serialport.isOpen()).to.be.true
+      })
     })
 
-    xit('should call messageCallback with XBee frame', function () {
-
+    it('should call callback with Error on XBee error', function (done) {
+      options.callback = function (error, message) {
+        expect(error).to.be.instanceof(Error)
+        expect(error).to.have.property('message', 'test')
+        done()
+      }
+      XBee.create(VirtualSerialPort, XBeeAPI, options).then(function (xbee) {
+        xbee.xbeeAPI.emit('error', Error('test'))
+      })
     })
-  })
 
-  describe('xbee.end()', function () {
-    xit('should resolve once the serial port is closed', function () {
-
-    })
-
-    xit('should resolve even if serialport is not open', function () {
-
+    it('should call messageCallback with XBee frame', function (done) {
+      options.callback = function (error, frame) {
+        expect(error).to.be.null
+        expect(frame).to.equal('test-frame')
+        done()
+      }
+      XBee.create(VirtualSerialPort, XBeeAPI, options).then(function (xbee) {
+        xbee.xbeeAPI.emit('frame_object', 'test-frame')
+      })
     })
   })
 
